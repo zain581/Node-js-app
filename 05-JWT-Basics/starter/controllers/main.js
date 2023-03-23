04ea061f76de3b1f0f99255a5bc1b907b859f3bc
zain581 <az4860364@gmail.com>
2023-03-23 06:48:28 -0400

    jwt

---
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -0,0 +1,43 @@
+// check username, password in post(login) request
+// if exist create new JWT
+// send back to front end
+
+// setup authentication so only the request with JWT can access dashboard
+
+const jwt = require('jsonwebtoken');
+const { BadRequestError } = require('../errors');
+
+const login = async (req, res) => {
+    const { username, password } = req.body;
+    // When user not provided username or password, we need to throw an error: by using these 3 methods:
+    // 1. mongoose validation
+    // 2. Joi - package name
+    // 3. check in the controller
+
+    if (!username || !password) {
+        throw new BadRequestError('Please provide email and password');
+    }
+
+    // just for demo, we will use a fake id
+    const id = new Date().getDate();
+
+    // try to keep the payload as small as possible, better for user experience
+    const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+        expiresIn: '30d',
+    });
+
+    res.status(200).json({ msg: 'user created', token });
+};
+
+const dashboard = async (req, res) => {
+    const luckyNumber = Math.floor(Math.random() * 100);
+    res.status(200).json({
+        msg: `Hello, ${req.user.username}`,
+        secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
+    });
+}
+
+module.exports = {
+    login,
+    dashboard,
+}
