cae5bf42db1ae52cb448ea387399b1f211d9c171
zain581 <az4860364@gmail.com>
2023-03-25 06:48:17 -0400

    api jobs

---
+++ 06-Jobs-API/starter/models/User.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please enter a name'],
+        minlength: 3,
+        maxlength: 50,
+    },
+    email: {
+        type: String,
+        required: [true, 'Please enter an email'],
+        match: [
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            'Please enter a valid email'
+        ],
+        unique: true,
+        lowercase: true,
+    },
+    password: {
+        type: String,
+        required: [true, 'Please enter a password'],
+        minlength: 5,
+    },
+});
+
+userSchema.pre('save', async function () {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
+
+userSchema.methods.createJWT = function () {
+    return jwt.sign(
+        {
+            userId: this._id,
+            name: this.name
+        },
+        process.env.JWT_SECRET,
+        {
+            expiresIn: process.env.JWT_LIFETIME,
+        }
+    );
+}
+
+userSchema.methods.comparePassword = async function (canditatePassword) {
+    const isMatch = await bcrypt.compare(canditatePassword, this.password);
+    return isMatch;
+}
+
+module.exports = mongoose.model('users', userSchema);
